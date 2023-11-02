5fb7a7f16abb591cd610cdf8109fa6ab36fe661d
test <mauro.henrique613@gmail.com>
2023-11-02 20:45:57 -0300

    Removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 import './App.css'
 {
   /* The following line can be included in your src/index.js or App.js file */
